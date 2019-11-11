05a0e091ba3bc72e22d6febc651320abcc2b5125
memesoundboard <50186369+absolute-music@users.noreply.github.com>
2019-11-11 12:36:29 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const dl = require("discord.lib");
+const client = dl.client;
+const Discord = dl.Discord;
 
-dl.sendMessage("A_help", "Help command!")
-dl.sendMessage("A_ping", "pong!")
-dl.embed("embed", "Test", "yo!", "#C70039")
+dl.sendMessage("help", "Help command!")
+dl.sendMessage("ping", "pong!")
 dl.joinMessage("633412217844269046", "Welcome!")
 dl.login("token", "I am the best bot!")
